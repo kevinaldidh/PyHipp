3790ac359c588a24fd87b59b4dacfbb6c1bd4d1c
kevinaldidh <98439053+kevinaldidh@users.noreply.github.com>
2022-10-31 17:35:17 +0800

    Update freq-slurm.sh

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -13,7 +13,7 @@
 #SBATCH -e freq-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-python -u -c "import PyHipp as pyh; \
+python -u -c "import PyHipp as pyh"; \
 import time; \
 pyh.RPLLFP(saveLevel=1); \
 print(time.localtime());
